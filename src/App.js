588a2c3ab90a028079dd8568e50730c2f59d463a
madhurjyabora1 <mrbora10@gmail.com>
2020-07-24 09:48:40 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Link, BrowserRouter as Router } from "react-router-dom";
+const colors = [
+  "palevioletred",
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "orange",
+  "lightblue",
+  "maroon",
+  "DarkCyan"
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const randomColor = items => {
+  return items[randomHeight(0, items.length)];
+};
+
+const randomHeight = (min, max) => {
+  return Math.floor(min + Math.random() * (max - min + 1));
+};
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addActiveClass = this.addActiveClass.bind(this);
+    this.state = {
+      activeIndex: -1
+    };
+  }
+  addActiveClass(activeIndex) {
+    this.setState(prev => ({
+      activeIndex: prev.activeIndex === activeIndex ? -1 : activeIndex
+    }));
+  }
+
+  render() {
+    return (
+      <Router>
+      <div className="container">
+        {Array.from({ length: 30 }).map((item, index) => {
+          return (
+            <Link to={`/${index}`}>
+            <div
+              key={index}
+              style={{
+                background: randomColor(colors),
+                height: randomHeight(100, 200)
+              }}
+              className={this.state.activeIndex === index ? "full" : "normal"}
+              onClick={() => this.addActiveClass(index)}
+            />
+              </Link>
+          );
+        })}
+      </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
